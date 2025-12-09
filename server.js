7553cf5377d9a8dd33df07ef3560971a428c3118
Benaya Giuseppe Linggaputra Sinulingga <Benaya.gls@gmail.com>
2025-12-09 10:24:55 +0700

    Backend Service

--- server.js
+++ server.js
@@ -6,28 +6,28 @@ const admin = require("firebase-admin");
 
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoute");
+const serviceRoute = require('./routes/serviceRoute'); 
 const startChatBot = require("./services/chatService");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-process.env.TZ = "Asia/Jakarta";
 
 const serviceAccount = require("./cema-web-firebase-adminsdk-fbsvc-7db7e59049.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL:
-    "https://cema-web-default-rtdb.asia-southeast1.firebasedatabase.app",
-});
-const db = admin.database();
+const app = express(); 
+const PORT = process.env.PORT || 5000;
+process.env.TZ = "Asia/Jakarta";
 
 connectDB();
 
-//Middleware
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://cema-web-default-rtdb.asia-southeast1.firebasedatabase.app",
+});
+const db = admin.database();
+
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api", authRoutes);
+app.use("/api", authRoutes); 
+app.use("/api/services", serviceRoute);
 
 startChatBot(db);
 
