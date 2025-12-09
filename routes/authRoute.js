557b1a8b813c15062851b0eb4cbf03fae2fd6c48
Jidan Azizi <idanaf3@gmail.com>
2025-12-09 09:26:34 +0700

    CRUD added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,7 +6,5 @@ const portfolioController = require("../controllers/portofolioController");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.get("/portfolio", portfolioController.getAllPortfolio); //get ALl
-router.get("/portfolio/:id", portfolioController.getPortfolioById); //get by ID
 
 module.exports = router;
