3ef8a1d8302ebe393808715d75fe46059b7903b7
Nicholas Irawan <nicholas2815@hotmail.com>
2025-12-19 01:56:48 +0700

    Revise Portfolio

--- routes/portfolioRoute.js
+++ routes/portfolioRoute.js
@@ -6,24 +6,6 @@ const portfolioController = require("../controllers/portofolioController");
 const authMiddleware = require("../middleware/authMiddleware");
 const checkPermission = require("../middleware/abacMiddleware");
 
-// MAKE SURE THERE'S NO CONFIG ERROR
-const multer = require("multer"); // Assuming multer was used/needed for upload, or if imported globally? The file had 'upload' but no import shown in the 13 lines snippet.
-// Wait, the snippet didn't show 'const upload = ...'. It must be hidden or missing?
-// "upload" variable is used but not defined in the top 13 lines viewed.
-// I should probably check if 'upload' is defined. If not, this code might be broken or I missed lines.
-// Re-reading file view... Total Lines: 13.
-// Lines 1-13 showed imports of express and portfolioController. No 'upload' config.
-// The code `router.post("/portfolio", upload.single("photoUrl")...` uses 'upload'.
-// If 'upload' is missing, this file is broken.
-// I will blindly assume I should NOT remove 'upload' usage, but if I don't see definition, it's risky.
-// However, my task is to ADD Auth/ABAC.
-// I will import middlewares and wrap the routes.
-
-// NOTE: Since I can't see 'upload' definition, I'll assume it's there or I should leave it be.
-// But wait, if I replace the lines, I need to keep 'upload'.
-// Let's assume 'const upload = require(...)' was meant to be there or is needed.
-// I'll add the imports at the top and modify the routes.
-
 //get Portfolio
 router.get(
   "/portfolio",
