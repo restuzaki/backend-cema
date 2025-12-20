e5a53cb2523f82639de0aeea9ad68349095b9402
Benaya Giuseppe Linggaputra Sinulingga <Benaya.gls@gmail.com>
2025-12-21 00:28:41 +0700

    adding logic for google login

--- models/User.js
+++ models/User.js
@@ -8,12 +8,26 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true,
+  },
+  googleId: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
+  name: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
   },
   role: {
     type: String,
     enum: ["client", "admin", "project_manager", "staff"],
     default: "client",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
