8f2b31515c0a20fc38c0728c4313d7694dc3a601
Damai <damaiputrayudha1@gmail.com>
2026-01-05 02:03:43 +0700

    fix upload folder

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // ROUTES (TETAP SAMA)
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 app.use("/api", authRoutes);
 app.use("/api", projectRoutes);
